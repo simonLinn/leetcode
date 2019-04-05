354511226d22a3c32dd49273dee45de78367dccd
simonLinn <simonswawa@gmail.com>
2019-04-05 14:12:09 +0800

    updata github

--- easy/1TwoSum.py
+++ easy/1TwoSum.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        solution=[0,0]
+        
         size=len(nums)
         d={nums[i] :i for i in range(size)  }
-        tmp=0
+        
         for i in range(size):
             tmp=target-nums[i]
             if(tmp in d):
-                if(d[tmp]!=i):
-                    solution[0]=i
-                    solution[1]=d[tmp]
-                    break
-
-        return solution
+                if (d[tmp]!=i):
+                    return [i,d[tmp]]

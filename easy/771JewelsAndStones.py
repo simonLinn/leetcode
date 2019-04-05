e4b9f1b3c315485cabdfe22e0d6a906bcd0f61ce
simonLinn <simonswawa@gmail.com>
2019-04-05 11:42:02 +0800

    updata github

--- easy/771JewelsAndStones.py
+++ easy/771JewelsAndStones.py
@@ -1,10 +1,8 @@
-def numJewelsInStones(J: str, S: str) -> int:
-    count = 0
-    for lj in J:
-        for ls in S:
-            if lj==ls:
-                count+=1
-    return count
-J = "aA"
-S = "aAAbbbb"
-print(numJewelsInStones(J,S))
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        for lj in J:
+            for ls in S:
+                if lj==ls:
+                    count+=1
+        return count
